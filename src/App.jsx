cc9125430c5dbcb11b5a5592402aad33b92cb619
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-10-07 19:32:51 -0300

    Refatorar estilos da página e adicionar seção de comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,126 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [author, setAuthor] = useState("")
+  const [content, setContent] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const newComment = {
+      id: Math.floor(Math.random() * 1000000),
+      author: author,
+      content: content,
+      createdAt: new Date() 
+    }
+  }
+
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="app">
+      <h2>Seção de Comentários</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="author">Email</label>
+        <input type="text" id="author" required 
+        value={author} onChange={(e)=>setAuthor(e.target.value)}/>
+        <label htmlFor="content">Comentário</label>
+
+        <textarea id="content" cols="30" rows="6" required
+        value={content} onChange={()=>setContent(e.target.value)}
+        ></textarea>
+        <button>Enviar comentário</button>
+      </form>
+      <hr />
+      <section id="comments">
+        <div>
+          <h3>autor@email.com</h3>
+          <span>Em 01/01/2001</span>
+          <p>Comentário de exemplo</p>
+        </div>
+      </section>
+    </div>
   )
 }
 
-export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+
+// export default function App() {
+//   const [comments, setComments] = useState([])
+//   const [author, setAuthor] = useState("")
+//   const [content, setContent] = useState("")
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault()
+
+//     const newComment = {
+//       id: Math.floor(Math.random() * 1000000),
+//       author: author,
+//       content: content,
+//       createdAt: new Date()
+//     }
+
+// 		setComments(state => [newComment, ...state])
+//     setAuthor("")
+//     setContent("")
+//   }
+
+//   return (
+
+//     <div id="app">
+//       <h2>Seção de Comentários</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="author">Email</label>
+//         <input
+// 					type="text" id="author" required
+// 					value={author} onChange={(e) => setAuthor(e.target.value)}
+// 				/>
+//         <label htmlFor="content">Comentário</label>
+//         <textarea
+// 					id="content" cols="30" rows="5" required
+// 					value={content} onChange={(e) => setContent(e.target.value)}
+// 				></textarea>
+//         <button>Enviar comentário</button>
+//       </form>
+//       <hr />
+//       <section id="comments">
+//       {comments.length > 0
+//           ? (
+//             comments.map((comment) => (
+//               <div key={comment.id}>
+//                 <h3>{comment.author}</h3>
+//                 <span>Em {comment.createdAt.toLocaleString()}</span>
+//                 <p>{comment.content}</p>
+//               </div>
+//             )))
+//           : (
+//             <p>Seja o primeiro a comentar!</p>
+//           )}
+//       </section>
+//     </div>
+//   )
+// }

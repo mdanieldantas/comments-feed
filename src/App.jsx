14c5d72b472458eb371556bd9510d8ab8675cc3c
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-10-07 19:45:18 -0300

    Refatorar estilos da página e adicionar seção de comentários

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 export default function App() {
+  const [comments, setComments] = useState([])
   const [author, setAuthor] = useState("")
   const [content, setContent] = useState("")
 
@@ -13,8 +14,12 @@ export default function App() {
       content: content,
       createdAt: new Date() 
     }
+    console.log({newComment})
+    
+    setComments([newComment, ...comments])
+    setAuthor("")
+    setContent("")
   }
-
   
 
   return (
@@ -27,12 +32,24 @@ export default function App() {
         <label htmlFor="content">Comentário</label>
 
         <textarea id="content" cols="30" rows="6" required
-        value={content} onChange={()=>setContent(e.target.value)}
+        value={content} onChange={(e)=>setContent(e.target.value)}
         ></textarea>
         <button>Enviar comentário</button>
       </form>
       <hr />
+
       <section id="comments">
+      {comments.length > 0 ? (
+        comments.map((comment) => (
+          <div key={comment.id}>
+            <h3>{comment.author}</h3>
+            <span>Em {comment.createdAt.toLocaleString()}</span>
+            <p>{comment.content}</p>
+          </div>
+        ))
+      ) : (
+        <p>Seja o primeiro a comentar!</p>
+      )}
         <div>
           <h3>autor@email.com</h3>
           <span>Em 01/01/2001</span>
